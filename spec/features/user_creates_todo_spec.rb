2f4c2c1eff34a83cf90771185850f393dee6a742
Hunt Norment <hunt@sherpa.red>
2016-06-19 11:50:05 -0500

    add todo

---
+++ spec/features/user_creates_todo_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+feature 'User creates todo' do 
+	scenario 'successfully' do
+		visit root_path
+
+		click_on 'Add a new Todo'
+
+		fill_in 'Title', with: 'Buy gift for FD'
+		click_button 'Submit'
+
+		expect(page).to have_css '.todos li', text: 'Buy gift for FD'
+	end
+end
